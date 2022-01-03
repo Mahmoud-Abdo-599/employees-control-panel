b1a4864cd0d86c2f5a2ec52b012309f84175694b
Mahmoud Abdo <mahmoud.abdo59986@gmail.com>
2022-01-03 21:20:02 +0200

    eject react scripts and apply antd configurations

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./assets/styles/_antd.less";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  import("./assets/styles/main.scss").then(() => {
+    require("./AppRender");
+  });
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render();
